45a44d48736c747ae26021fa2425d16fcc7e559e
Ariel Batoon <info.arielbatoon@gmail.com>
2022-08-27 01:57:47 +0800

    Second Update

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,23 +2,13 @@ $(function(){
     $(".hamburger").click(function(){
         $('.hamburger').toggleClass('active');
     });
-
     $(window).on('scroll', function(){
         var scroll = $(window).scrollTop();
         if(scroll >= 50){
-            $('.bar').addClass('hambuger-scrolled');
-            $('nav').addClass('stickyadd');
             $('nav').removeClass('stick');
         }
         else{
-            $('.bar').removeClass('hambuger-scrolled');
-            $('nav').removeClass('stickyadd');
             $('nav').addClass('stick');
         }
     });
-
-    particlesJS.load('particles-js', 'assets/js/particles.json', function() {
-        console.log('callback - particles.js config loaded');
-      });
-
 });
